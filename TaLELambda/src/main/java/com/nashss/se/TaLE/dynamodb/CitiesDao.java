d4ba366d07a09f1c978c4da344a0b16b89fcd827
Justin Locke <Justindeanlocke@gmail.com>
2023-11-09 16:19:02 -0600

    Added Metrics Publisher and Finished Cities DAO

--- TaLELambda/src/main/java/com/nashss/se/TaLE/dynamodb/CitiesDao.java
+++ TaLELambda/src/main/java/com/nashss/se/TaLE/dynamodb/CitiesDao.java
@@ -1,4 +1,39 @@
 package com.nashss.se.TaLE.dynamodb;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.nashss.se.TaLE.dynamodb.models.City;
+import com.nashss.se.TaLE.metrics.MetricsPublisher;
+
+import javax.inject.Inject;
+
 public class CitiesDao {
+    private final DynamoDBMapper dynamoDBMapper;
+    private final MetricsPublisher metricsPublisher;
+
+    /**
+     * Instantiates a CitiesDao object.
+     * @param dynamoDBMapper used to interact with the cities table.
+     * @param metricsPublisher used to publish metrics to CloudWatch.
+     */
+    @Inject
+    public CitiesDao(DynamoDBMapper dynamoDBMapper, MetricsPublisher metricsPublisher) {
+        this.dynamoDBMapper = dynamoDBMapper;
+        this.metricsPublisher = metricsPublisher;
+    }
+
+    public City getCityById(String id) {
+        City city = this.dynamoDBMapper.load(City.class, id);
+
+        if (city == null) {
+
+        }
+
+        return city;
+    }
+
+    public City saveCity(City newCity) {
+        this.dynamoDBMapper.save(newCity);
+        return newCity;
+    }
+
 }
