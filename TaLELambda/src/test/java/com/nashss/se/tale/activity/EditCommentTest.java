f3e56b35139fa80cfd036e0e30d8f731b2a291f3
Justin Locke <Justindeanlocke@gmail.com>
2023-12-11 21:10:24 -0600

    Updated Tests to show possible exception throws

--- TaLELambda/src/test/java/com/nashss/se/tale/activity/EditCommentTest.java
+++ TaLELambda/src/test/java/com/nashss/se/tale/activity/EditCommentTest.java
@@ -4,6 +4,8 @@ import com.nashss.se.tale.activity.requests.EditCommentRequest;
 import com.nashss.se.tale.activity.results.EditCommentResult;
 import com.nashss.se.tale.dynamodb.CommentsDao;
 import com.nashss.se.tale.dynamodb.models.Comment;
+import com.nashss.se.tale.exceptions.EmptyFieldInRequestException;
+import com.nashss.se.tale.exceptions.InvalidUserException;
 import com.nashss.se.tale.models.CommentModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,11 +32,14 @@ class EditCommentTest {
     @Test
     void handleRequest_withValidFields_returnsCorrectResult() {
         //Given a valid request
+        String oldCommentTitle = "Sad Day";
+        String oldCommentMessage = "I'm so sad today.";
+
         Comment oldComment = new Comment();
         oldComment.setCommentId("CM923");
         oldComment.setEdited(false);
-        oldComment.setTitle("Sad Day");
-        oldComment.setMessage("I'm so sad today.");
+        oldComment.setTitle(oldCommentTitle);
+        oldComment.setMessage(oldCommentMessage);
         oldComment.setDatePosted(LocalDate.now());
         oldComment.setActivityId("ACT1");
         oldComment.setUserId("JLy03");
@@ -56,7 +61,88 @@ class EditCommentTest {
         assertEquals(oldComment.getActivityId(), model.getActivityId());
         assertEquals(oldComment.getDatePosted(), model.getDatePosted());
         assertEquals(oldComment.getUserId(), model.getUserId());
-        assertNotEquals("Sad Day", model.getTitle());
-        assertNotEquals("I'm so sad today.", model.getMessage());
+        assertNotEquals(oldCommentTitle, model.getTitle());
+        assertNotEquals(oldCommentMessage, model.getMessage());
     }
+
+    @Test
+    void handleRequest_withEmptyTitle_throwsEmptyFieldInRequestException() {
+        //Given a valid request
+        String oldCommentTitle = "Sad Day";
+        String oldCommentMessage = "I'm so sad today.";
+
+        Comment oldComment = new Comment();
+        oldComment.setCommentId("CM923");
+        oldComment.setEdited(false);
+        oldComment.setTitle(oldCommentTitle);
+        oldComment.setMessage(oldCommentMessage);
+        oldComment.setDatePosted(LocalDate.now());
+        oldComment.setActivityId("ACT1");
+        oldComment.setUserId("JLy03");
+        EditCommentRequest request = new EditCommentRequest.Builder()
+                .withActivityId("ACT1")
+                .withCommentId("CM923")
+                .withUserId("JLy03")
+                .withTitle("")
+                .withMessage("I'm so happy today.")
+                .build();
+        //When
+        //Then
+        assertThrows(EmptyFieldInRequestException.class,
+                () -> editComment.handleRequest(request));
+    }
+
+    @Test
+    void handleRequest_withEmptyMessage_throwsEmptyFieldInRequestException() {
+        //Given a valid request
+        String oldCommentTitle = "Sad Day";
+        String oldCommentMessage = "I'm so sad today.";
+
+        Comment oldComment = new Comment();
+        oldComment.setCommentId("CM923");
+        oldComment.setEdited(false);
+        oldComment.setTitle(oldCommentTitle);
+        oldComment.setMessage(oldCommentMessage);
+        oldComment.setDatePosted(LocalDate.now());
+        oldComment.setActivityId("ACT1");
+        oldComment.setUserId("JLy03");
+        EditCommentRequest request = new EditCommentRequest.Builder()
+                .withActivityId("ACT1")
+                .withCommentId("CM923")
+                .withUserId("JLy03")
+                .withTitle("Happy Day")
+                .withMessage("")
+                .build();
+    assertThrows(EmptyFieldInRequestException.class,
+                () -> editComment.handleRequest(request));
+}
+
+    @Test
+    void handleRequest_withIncorrectUserId_throwsInvalidUserException() {
+        //Given a valid request
+        String oldCommentTitle = "Sad Day";
+        String oldCommentMessage = "I'm so sad today.";
+
+        Comment oldComment = new Comment();
+        oldComment.setCommentId("CM923");
+        oldComment.setEdited(false);
+        oldComment.setTitle(oldCommentTitle);
+        oldComment.setMessage(oldCommentMessage);
+        oldComment.setDatePosted(LocalDate.now());
+        oldComment.setActivityId("ACT1");
+        oldComment.setUserId("JLy03");
+        EditCommentRequest request = new EditCommentRequest.Builder()
+                .withActivityId("ACT1")
+                .withCommentId("CM923")
+                .withUserId("JPb03")
+                .withTitle("Happy Day")
+                .withMessage("I'm so happy today.")
+                .build();
+        //When
+        when(commentsDao.getComment(anyString(), anyString())).thenReturn(oldComment);
+        //Then
+        assertThrows(InvalidUserException.class,
+                () -> editComment.handleRequest(request));
+    }
+
 }
