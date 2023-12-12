8e6c635af9b2c55b06581c49375610bce6b45d85
Justin Locke <Justindeanlocke@gmail.com>
2023-12-12 11:21:00 -0600

    Updated imports to not use *

--- TaLELambda/src/main/java/com/nashss/se/tale/dynamodb/models/Comment.java
+++ TaLELambda/src/main/java/com/nashss/se/tale/dynamodb/models/Comment.java
@@ -1,6 +1,15 @@
 package com.nashss.se.tale.dynamodb.models;
-import com.amazonaws.services.dynamodbv2.datamodeling.*;
 import com.nashss.se.tale.converters.LocalDateConverter;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIndexHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIndexRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperFieldModel;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTyped;
 
 import java.time.LocalDate;
 import java.util.Objects;
